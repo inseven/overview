2e433a7e81be1c6d91da29d9d554050db4fc3f4b
Jason Barrie Morley <jbmorley@mac.com>
2021-03-13 10:29:52 -0800

    Initial experimentation for a new way to calculate date summaries.

--- Where/Where/Interface/MonthView.swift
+++ Where/Where/Interface/MonthView.swift
@@ -8,6 +8,35 @@
 import EventKit
 import SwiftUI
 
+extension EKEvent {
+
+    var duration: TimeInterval {
+        return endDate.timeIntervalSince(startDate)
+    }
+
+    var components: DateComponents {
+        // TODO: Use the calendar from the calendar?
+        // TODO: More components?
+        return Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: startDate, to: endDate)
+    }
+
+}
+
+extension Summary where Item: EKEvent {
+
+    var duration: TimeInterval {
+        Set(items).reduce(0.0) { total, event in total + event.duration }
+    }
+
+    var components: DateComponents {
+        guard let event = items.first else {
+            return DateComponents()
+        }
+        return event.components
+    }
+
+}
+
 struct MonthView: View {
 
     @State var summary: Summary<Summary<EKEvent>>
@@ -16,6 +45,13 @@ struct MonthView: View {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMMM"
         return dateFormatter
+    }()
+
+    var dateComponentsFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.calendar = Calendar.current  // TODO: Is this needed?
+        formatter.unitsStyle = .full
+        return formatter
     }()
 
     var title: String { dateFormatter.string(from: summary.dateInterval.start) }
@@ -40,7 +76,9 @@ struct MonthView: View {
                         Text(summary.items[0].title ?? "Unknown")
                             .lineLimit(1)
                         Spacer()
-                        Text("\(summary.items.count)")
+                        Text("\(summary.items.count) events")
+                        Text("\(dateComponentsFormatter.string(from: summary.duration) ?? "Cheese")")
+//                        Text("\(dateComponentsFormatter.string(from: summary.components) ?? "Cheese")")
                     }
                 }
                 Divider()
