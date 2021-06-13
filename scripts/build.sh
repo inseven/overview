22ab70160b40685104412230cc2b2914da7e9f16
Jason Morley <jason.morley@inseven.co.uk>
2021-06-13 07:46:18 -0700

    ci: Make use of the PATH to simplify the build script (#16)

--- scripts/build.sh
+++ scripts/build.sh
@@ -35,11 +35,13 @@ KEYCHAIN_PATH="${TEMPORARY_DIRECTORY}/temporary.keychain"
 ARCHIVE_PATH="${BUILD_DIRECTORY}/Overview.xcarchive"
 FASTLANE_ENV_PATH="${ROOT_DIRECTORY}/fastlane/.env"
 
-CHANGES_SCRIPT="${SCRIPTS_DIRECTORY}/changes/changes"
-CHANGES_GITHUB_RELEASE_SCRIPT="${SCRIPTS_DIRECTORY}/changes/examples/gh-release.sh"
-BUILD_TOOLS_SCRIPT="${SCRIPTS_DIRECTORY}/build-tools/build-tools"
+CHANGES_DIRECTORY="${SCRIPTS_DIRECTORY}/changes"
+BUILD_TOOLS_DIRECTORY="${SCRIPTS_DIRECTORY}/build-tools"
 
-MACOS_PROJECT_PATH="${ROOT_DIRECTORY}/macos/Overview.xcodeproj"
+CHANGES_GITHUB_RELEASE_SCRIPT="${CHANGES_DIRECTORY}/examples/gh-release.sh"
+
+PATH=$PATH:$CHANGES_DIRECTORY
+PATH=$PATH:$BUILD_TOOLS_DIRECTORY
 
 # Process the command line arguments.
 POSITIONAL=()
@@ -77,14 +79,14 @@ if [ -f "$FASTLANE_ENV_PATH" ] ; then
     source "$FASTLANE_ENV_PATH"
 fi
 
-function xcode {
+function xcode_project {
     xcodebuild \
-        -project "$MACOS_PROJECT_PATH" "$@"
+        -project macos/Overview.xcodeproj "$@"
 }
 
 function build_scheme {
     # Disable code signing for the build server.
-    xcode \
+    xcode_project \
         -scheme "$1" \
         CODE_SIGN_IDENTITY="" \
         CODE_SIGNING_REQUIRED=NO \
@@ -94,7 +96,7 @@ function build_scheme {
 cd "$ROOT_DIRECTORY"
 
 # List the available schemes
-xcode -list
+xcode_project -list
 
 # Smoke test builds.
 
@@ -114,19 +116,19 @@ if [ -d "$TEMPORARY_DIRECTORY" ] ; then
     rm -rf "$TEMPORARY_DIRECTORY"
 fi
 mkdir -p "$TEMPORARY_DIRECTORY"
-echo "$TEMPORARY_KEYCHAIN_PASSWORD" | "$BUILD_TOOLS_SCRIPT" create-keychain "$KEYCHAIN_PATH" --password
+echo "$TEMPORARY_KEYCHAIN_PASSWORD" | build-tools create-keychain "$KEYCHAIN_PATH" --password
 
 function cleanup {
     # Cleanup the temporary files and keychain.
     cd "$ROOT_DIRECTORY"
-    "$BUILD_TOOLS_SCRIPT" delete-keychain "$KEYCHAIN_PATH"
+    build-tools delete-keychain "$KEYCHAIN_PATH"
     rm -rf "$TEMPORARY_DIRECTORY"
 }
 
 trap cleanup EXIT
 
 # Determine the version and build number.
-VERSION_NUMBER=`"$CHANGES_SCRIPT" --scope macOS version`
+VERSION_NUMBER=`changes --scope macOS version`
 GIT_COMMIT=`git rev-parse --short HEAD`
 TIMESTAMP=`date +%s`
 BUILD_NUMBER="${GIT_COMMIT}.${TIMESTAMP}"
@@ -135,8 +137,19 @@ BUILD_NUMBER="${GIT_COMMIT}.${TIMESTAMP}"
 fastlane import_certificates keychain:"$KEYCHAIN_PATH"
 
 # Archive and export the build.
-xcode -scheme "Overview" -config Release -archivePath "$ARCHIVE_PATH"  OTHER_CODE_SIGN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\"" BUILD_NUMBER=$BUILD_NUMBER MARKETING_VERSION=$VERSION_NUMBER archive | xcpretty
-xcodebuild -archivePath "$ARCHIVE_PATH" -exportArchive -exportPath "$BUILD_DIRECTORY" -exportOptionsPlist "macos/ExportOptions.plist"
+xcode_project \
+    -scheme "Overview" \
+    -config Release \
+    -archivePath "$ARCHIVE_PATH" \
+    OTHER_CODE_SIGN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\"" \
+    BUILD_NUMBER=$BUILD_NUMBER \
+    MARKETING_VERSION=$VERSION_NUMBER \
+    archive | xcpretty
+xcodebuild \
+    -archivePath "$ARCHIVE_PATH" \
+    -exportArchive \
+    -exportPath "$BUILD_DIRECTORY" \
+    -exportOptionsPlist "macos/ExportOptions.plist"
 
 APP_BASENAME="Overview.app"
 APP_PATH="$BUILD_DIRECTORY/$APP_BASENAME"
@@ -153,12 +166,18 @@ fi
 pushd "$BUILD_DIRECTORY"
 ZIP_BASENAME="Overview-macOS-${VERSION_NUMBER}.zip"
 zip -r --symlinks "$ZIP_BASENAME" "$APP_BASENAME"
-"$BUILD_TOOLS_SCRIPT" verify-notarized-zip "$ZIP_BASENAME"
+build-tools verify-notarized-zip "$ZIP_BASENAME"
 rm -r "$APP_BASENAME"
 zip -r "Artifacts.zip" "."
 popd
 
 # Attempt to create a version tag and publish a GitHub release; fails quietly if there's no new release.
 if $RELEASE || $TRY_RELEASE ; then
-    "$CHANGES_SCRIPT" --scope macOS release --skip-if-empty --push --command "\"${CHANGES_GITHUB_RELEASE_SCRIPT}\" \"\$@\"" "${BUILD_DIRECTORY}/${ZIP_BASENAME}"
+    changes \
+        --scope macOS \
+        release \
+        --skip-if-empty \
+        --push \
+        --command "\"${CHANGES_GITHUB_RELEASE_SCRIPT}\" \"\$@\"" \
+        "${BUILD_DIRECTORY}/${ZIP_BASENAME}"
 fi
