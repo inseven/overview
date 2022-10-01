6a525da22a900d61614293ddc8515755ddbaa07f
Jason Barrie Morley <jbmorley@mac.com>
2022-09-30 18:54:27 -0700

    refactor: Complete rename of some of the test files from 'Where' to 'Overview'

--- macos/OverviewTests/OverviewTests.swift
+++ macos/OverviewTests/OverviewTests.swift
@@ -21,7 +21,7 @@
 import XCTest
 @testable import Overview
 
-class WhereTests: XCTestCase {
+class OverviewTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- macos/OverviewUITests/OverviewUITests.swift
+++ macos/OverviewUITests/OverviewUITests.swift
@@ -20,7 +20,7 @@
 
 import XCTest
 
-class WhereUITests: XCTestCase {
+class OverviewUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
