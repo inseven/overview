883bcac56806e330011d141b01f7846534a5d7af
Jason Morley <hello@jbmorley.co.uk>
2023-08-25 17:34:07 -1000

    ci: Update the App Store Connect API key

--- scripts/build.sh
+++ scripts/build.sh
@@ -109,15 +109,11 @@ echo "$TEMPORARY_KEYCHAIN_PASSWORD" | build-tools create-keychain "$KEYCHAIN_PAT
 
 function cleanup {
 
-    # Cleanup the temporary files and keychain.
+    # Cleanup the temporary files, keychain and keys.
     cd "$ROOT_DIRECTORY"
     build-tools delete-keychain "$KEYCHAIN_PATH"
     rm -rf "$TEMPORARY_DIRECTORY"
-
-    # Clean up any private keys.
-    if [ -f ~/.appstoreconnect/private_keys ]; then
-        rm -r ~/.appstoreconnect/private_keys
-    fi
+    rm -r ~/.appstoreconnect/private_keys
 }
 
 trap cleanup EXIT
@@ -153,6 +149,10 @@ APP_BASENAME="Overview.app"
 APP_PATH="$BUILD_DIRECTORY/$APP_BASENAME"
 PKG_PATH="$BUILD_DIRECTORY/Overview.pkg"
 
+# Install the private key.
+mkdir -p ~/.appstoreconnect/private_keys/
+echo -n "$APPLE_API_KEY_BASE64" | base64 --decode -o ~/".appstoreconnect/private_keys/AuthKey_${APPLE_API_KEY_ID}.p8"
+
 # Validate the macOS build.
 xcrun altool --validate-app \
     -f "${PKG_PATH}" \
@@ -169,10 +169,6 @@ zip -r "${ZIP_BASENAME}" .
 popd
 
 if $RELEASE ; then
-
-    # Install the private key.
-    mkdir -p ~/.appstoreconnect/private_keys/
-    echo -n "$APPLE_API_KEY" | base64 --decode -o ~/".appstoreconnect/private_keys/AuthKey_${APPLE_API_KEY_ID}.p8"
 
     changes \
         release \
